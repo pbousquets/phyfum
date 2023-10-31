3bdae442f15df8e2735eb28475a31287b9d13f7c
Pablo Bousquets-Muñoz <bousquetspablo@uniovi.es>
2023-10-31 10:45:05 +0100

    Fix checks and lint variables

--- src/phyfum/workflow/aux/checkPatients.py
+++ src/phyfum/workflow/aux/checkPatients.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from typing import List
 
 
-def checkPatientsBetas(patients: list[str], dir: str) -> list[str]:
+def checkPatientsBetas(patients: List[str], dir: str) -> List[str]:
     validPatients = []
     for patient in patients:
         csv_file = f"{dir}/{patient}.csv"
@@ -10,7 +11,7 @@ def checkPatientsBetas(patients: list[str], dir: str) -> list[str]:
         if len(df.columns) >= 3:
             validPatients.append(patient)
     if len(validPatients) == 0:
-        raise ValueError("No patients with at least 3 samples were found! Please check your input data.")
+        raise ValueError(f"No patients with at least 3 samples were found! Please check your input data. {df.columns}")
 
     return validPatients
 
@@ -19,3 +20,4 @@ def checkPatientInfo(patientInfo, patient_col):
     patientInfo = patientInfo[patientInfo.groupby(patient_col)[patient_col].transform('count') >= 3]
     if len(patientInfo) == 0:
         raise ValueError("No patients with at least 3 samples were found! Please check your input data.")
+    return patientInfo
